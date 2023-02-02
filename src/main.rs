27ee889bd2d24cfa106dc696ced377cee0098620
M-Farag <mina.farag@icloud.com>
2023-02-03 00:13:45 +0200

    UPD Simplify the code

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,8 @@ fn main() {
 
     println!("Define the two letters you wanna replace ?! x -with-> y");
     io::stdin().read_line(&mut user_replace_input).expect("Err Reading input");
-    let letters_to_replace:(char,char) = (' ',' ');
-    let letters_to_replace = chars_to_replace(user_replace_input,letters_to_replace);
-    println!("Change letter {} with a {}",letters_to_replace.0,letters_to_replace.1);
+    let letters_to_replace = chars_to_replace(user_replace_input);
+    println!("Change letter '{}' with a '{}' ",letters_to_replace.0,letters_to_replace.1);
 
     println!("Add the text you wanna change");
     io::stdin().read_line(&mut user_input_text).expect("Err reading your text");
@@ -19,28 +18,24 @@ fn main() {
 
 }
 
-fn chars_to_replace(user_replace_input:String,mut letters_container:(char, char)) -> (char,char)
+fn chars_to_replace(user_replace_input:String) -> (char,char)
 {
-    
-    letters_container.0 = user_replace_input.chars().take(1).last().unwrap();
-    letters_container.1 = user_replace_input.chars().take(3).last().unwrap();
-    letters_container
+    (  user_replace_input.chars().take(1).last().unwrap(),
+       user_replace_input.chars().take(3).last().unwrap() 
+    )
     
 }
 
 fn replace_text(letter_to_replace:(char, char), user_text:String) ->String
 {
-    let mut modified_text:String = String::new();
+    let mut modified_text:String = String::with_capacity(user_text.len());
     let letters = user_text.chars();
 
-    for letter in letters {
+    for mut letter in letters {
         if letter == letter_to_replace.0  {
-            modified_text.push(letter_to_replace.1);
+            letter = letter_to_replace.1;
         }
-        else {
-            modified_text.push(letter);
-        }
-        
+        modified_text.push(letter);
     }
     modified_text
 
